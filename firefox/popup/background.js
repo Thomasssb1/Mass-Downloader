ad8da8d798cc3936b6a7baefa66619f241c44d8e
Thomas Beer <thomas.beer04@outlook.com>
2023-06-03 15:42:27 +0100

    firefox seperation + fix

---
+++ firefox/popup/background.js
@@ -0,0 +1,37 @@
+const btn = document.getElementById('downloadCurrent');
+const settings = document.getElementById('settings');
+
+btn.addEventListener('click', async function onClick() {
+  var imgs = document.getElementsByTagName('img');
+  var imgSRCs = [];
+  for (var i = 0; i < imgs.length; i++) {
+    imgSRCs.push(imgs[i].src);
+  }
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tab) {
+    try {
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: tab[0]['id'] },
+          func: () => {
+            var imgs = document.getElementsByTagName('img');
+            var imgSRCs = [];
+            for (i = 0; i < imgs.length; i++) {
+              imgSRCs.push(imgs[i].src);
+            }
+            return imgSRCs;
+          },
+        },
+        (res) => {
+          browser.storage.local.set({ imgSRCs: res[0].result });
+          browser.tabs.create({ url: '../scripts/page.html' });
+        }
+      );
+    } catch (e) {
+      console.log(e.name);
+    }
+  });
+});
+
+settings.addEventListener('click', function onClick() {
+  browser.tabs.create({ url: 'about:preferences#general' });
+});
